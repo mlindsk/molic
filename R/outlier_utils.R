7ddb374e6714042d74a41d045e12ab34187c6866
mads <mads@math.aau.dk>
2019-03-26 15:49:38 +0100

    Complete makeover of simulation functions.

--- R/outlier_utils.R
+++ R/outlier_utils.R
@@ -35,9 +35,29 @@ weight_matrix <- function(df) {
 
 na <- function(df, a) {
   # The a- marginal table
-  if( !neq_empt_chr(a) ) stop("a is the empty set")
+  if( !neq_empt_chr(a) ) return(nrow(df)) # vars = NULL
   # Keep "a" as an attribute for lookup possibility
   structure(table(apply(df[, a], 1, paste0, collapse = "")), vars = a)
+}
+
+.split_chars <- function(x) unlist(strsplit(x, ""))
+
+na_b <- function(na, b) {
+  # The b'th slice in the a marginal table
+  # - b is a __NAMED__ vector of indexes pointing to the positions of the b levels
+  # - ATCGTT where (ATCGTT) is the clique and (ACG) is the separator
+  if( !neq_empt_num(b) ) return(na)
+  cells    <- names(na)
+  vars     <- attr(na, "vars")
+  .nc      <- length(vars)
+  fix_b    <- rep(".", .nc)
+  fix_b[b] <- names(b)
+  fix_b    <- paste0(fix_b, collapse = "")
+  .na_b    <- na[grepl(fix_b, cells)]
+  N        <- names(.na_b)
+  names(.na_b) <- sapply(N, function(f) paste0(.split_chars(f)[-b], collapse = ""))
+  attr(.na_b, "vars") <- vars[-b]
+  .na_b
 }
 
 na_ya <- function(na, ya) {
@@ -45,23 +65,6 @@ na_ya <- function(na, ya) {
   val <- unname(na[paste0(ya, collapse = "")]) 
   if(is.na(val)) return(0L) else return(val)
 }
-  
-
-na_b <- function(na, b) {
-  # The b'th slice in the a marginal table
-  # - b is a vector of strings (e.g. c("G", "T", "T"))
-  # - the names of na are assumed to be sorted occording to
-  # - ATCGTT where (ATCGTT) is the clique and (GTT) is the separator
-  if( !neq_empt_chr(b) ) return(na)
-  dimb  <- length(b)
-  vals  <- names(na)
-  nvals <- nchar(vals[1])
-  avals <- substr(vals, 1L, nvals - dimb)
-  bvals <- substr(vals, nvals - dimb + 1L, nvals)
-  bs    <- which(bvals == paste0(b, collapse = ""))
-  structure( na[bs], names = avals[bs], vars =  attr(na, "vars")[1:(nvals-dimb)])
-}
-
 
 a_marginals <- function(df, am) {
   # INPUT:
@@ -71,235 +74,86 @@ a_marginals <- function(df, am) {
   lapply( am, function(x) if(is.null(x)) return(x)  else return(na(df, x)) )  
 }
 
+xlogx <- function(x) { # G
+  x_ <- x > 0L
+  x[x_] <- x[x_]*log(x[x_])
+  x[!x_] <- 0L
+  x
+}
 
-## Let it output a function that can be used on y's
-## p_y <- function(df, y, C_marginals, S_marginals) {
-##   # INPUT:
-##   # C_marginals: marginal clique tables from function a_marginals
-##   # S_marginals: -
-  
-##   # OUTPUT:
-##   # Joint density of Y over clique marginals
-
-##   Delta   <- colnames(df) # Let it be an input?
-##   M       <- nrow(df)
-##   nC1_yC1 <- na_ya(Delta, C_marginals[[1]], y)
-##   nC_nS   <- 1
-##   if(length(C_marginals) - 1) {
-##     nC_nS   <- vapply(X = 2:length(C_marginals), FUN.VALUE = 1, FUN =  function(k) {
-##       nCk_yCk <- na_ya(Delta, C_marginals[[k]], y)
-##       nSk_ySk <- na_ya(Delta, S_marginals[[k]], y)
-##       ifelse(nSk_ySk > 0, nCk_yCk / nSk_ySk, 0)
-##     })  
-##   }
-##   nC1_yC1 / M * prod(nC_nS)
-## }
-
-G_ <- function(x) x*log(x)
-
-H_ <- function(x){
-  G_(x-1) - G_(x)
-} 
-
-## T_Yk <- function(df, y, k, C_marginal_k, S_marginal_k){ ## DONT DRAG df AROUND!!!
-##   # INPUT:
-##   # a_marginal_k: the k'th marginal table from an a_marginals call
-
-##   # OUTPUT:
-##   # Tk evaluated at Yk = yk
-##   Delta <- colnames(df) # Let it be an input?
-##   H_Ck  <- H_(na_ya(Delta, C_marginal_k ,y))
-##   if( k == 1 ) {
-##     return( H_Ck )
-##   } else {
-##     H_Sk  <- H_(na_ya(Delta, S_marginal_k ,y))
-##     return( H_Ck - H_Sk )
-##   }
-## }
-
-## T_Yk2 <- function(df, yk, k, C_marginal_k, S_marginal_k){ ## DONT DRAG df AROUND!!!
-##   # INPUT:
-##   # a_marginal_k: the k'th marginal table from an a_marginals call
-##   # yk: the marginal profile corresponding to the k'th marginal table(s)
-
-##   # OUTPUT:
-##   # Tk evaluated at Yk = yk
-##   browser()
-
-##   na_ya_k <- na_ya(C_marginal_k, paste0(yk, collapse = ""))
-
-##   H_Ck  <- H_(na_ya(Delta, C_marginal_k ,y))
-
-##   if( k == 1 ) {
-##     return( H_Ck )
-##   } else {
-##     H_Sk  <- H_(na_ya(Delta, S_marginal_k ,y))
-##     return( H_Ck - H_Sk )
-##   }
-## }
-
-## -------------------------------------------------------------
-## BOOKMARK!!!!                                                              
-## -------------------------------------------------------------
-
+x1logx1 <- function(x) xlogx(x-1) - xlogx(x) # H
 
 TY <- function(y, C_marginals, S_marginals) {
-  # y: named vector (observations)!!!
+  # y: _named_ vector/observation
   Cs <- sapply(C_marginals, function(x) {
     na_ya(x, paste0(y[attr(x, "vars")], collapse = ""))
   })
-
-  Ss <- sapply(S_marginals[-1], function(x) {
+  Ss <- if( length(S_marginals) >1) sapply(S_marginals[-1], function(x) {
     na_ya(x, paste0(y[attr(x, "vars")], collapse = ""))
-  })
-
-  # Resort 0*log(0) in vectorisez function
-  Ss <- c(0L, Ss)
-  H_(Cs) - c(0, H_(Ss))
+  }) else 0L
+  sum(x1logx1(Cs) - c(0L, x1logx1(Ss))) # H(C_yk) - H(S_yk)
 }
 
-
-## -------------------------------------------------------------
-## BOOKMARK                                                             
-## -------------------------------------------------------------
-
-
-T_Y <- function(df, y, C_marginals, S_marginals) { ## DONT DRAG df AROUND!!!
-  # OUTPUT:
-  # T evaluated at Y = y
-  Tks <- vapply(X = seq_along(C_marginals), FUN.VALUE = 1, FUN =  function(k) {
-    T_Yk(df, y, k, C_marginals[[k]], S_marginals[[k]])
-  })
-  sum(Tks)
-}
-
-
-E_TY <- function(df, C_marginals, S_marginals) {
-  # OUTPUT:
-  # Exptected value of T(Y)
-  
-  Delta  <- colnames(df)
-  y      <- vector("character", length(Delta))
-  M      <- nrow(df)
-
-  E <- vapply(X = seq_along(C_marginals), FUN.VALUE = 1, FUN = function(k) {
-    nSk       <- S_marginals[[k]]
-    nCk       <- C_marginals[[k]]
-    Ck_index  <- ia(Delta, names(dimnames(nCk)))
-    Ck_levels <- dimnames(nCk)
-    ICk   <- do.call(.expand_grid, Ck_levels)
-    E_Tk  <- vapply(X = 1:nrow(ICk), FUN.VALUE = 1, FUN =  function(i) {
-      y[Ck_index] <- unlist(ICk[i,])
-      Tk <- T_Yk(df, y, k, nCk, nSk)
-      Tk * na_ya(Delta, nCk, y) / M
-    })
-    sum(E_Tk)
-  })
-  sum(E)
-}
-
-
-V_TY <- function(df, C_marginals, S_marginals, E_ty) {
-  # INPUT:
-  # e_ty : A mean value obtained from E_TY
-
-  # OUTPUT:
-  # Variance of T(Y)
-  Delta  <- colnames(df)
-  I      <- do.call(.expand_grid, lapply(df, unique))
-  nI     <- nrow(I)
-  V      <- vector("numeric", length = nI)
-  for( i in 1:nI ) {
-    yi  <- unlist(I[i, ])
-    Ty  <- T_Y(df, yi, C_marginals, S_marginals)
-    V[i] <- (Ty - E_ty)^2 * p_y(df, yi, C_marginals, S_marginals)
-  }
-  sum(V)
-}
-
-sim_Y <- function(df,
+simulate_TY <- function(df,
                   C_marginals,
                   S_marginals,
                   n.sim = 1000,
                   ncores = 1,
                   verbose = TRUE) {
-
   # OUTPUT:
   # A simulated profile from the database df
-
   y <- replicate(n.sim, vector("character", ncol(df)), simplify = FALSE)
   M <- nrow(df)
-  
-  Delta     <- colnames(df)
-  p_nC1     <- C_marginals[[1]] / M
-  C1_index  <- ia(Delta, names(dimnames(p_nC1)))
-  C1_levels <- dimnames(p_nC1)
-
-  IC1     <- do.call(.expand_grid, C1_levels)
-  yC1_sim <- sample(1:nrow(IC1), n.sim, replace = TRUE, prob = c(p_nC1))
-
-  if(verbose) cat("\nSimulating... \n")
-  if( !verbose ) pbapply::pboptions(type = "none")
-  on.exit(pbapply::pboptions(type = "timer"))
-  
-  y <- pbapply::pblapply(X = 1:n.sim, cl = ncores, FUN =  function(z) {
+  Delta   <- colnames(df)
+  C1_vars <- attr(C_marginals[[1]], "vars")
+  C1_idx  <- match(C1_vars, Delta)
+  p_nC1   <- C_marginals[[1]] / M
+  yC1_sim <- sample(names(p_nC1), n.sim, replace = TRUE, prob = p_nC1)
+  ## importFrom(foreach, "%dopar%")
+  ## import::from(foreach, "%dopar%")
+  library(doParallel)
+  doParallel::registerDoParallel(ncores)
+  y <- foreach::foreach(z = 1:n.sim, .combine = 'c') %dopar% {
     y_sim_z <- y[[z]]
-    y_sim_z[C1_index] <- unlist(IC1[yC1_sim[z],])
-
-    # Handling components wich are complete (and thereby no separators)
-    if( length(S_marginals) == 1 ) return(y_sim_z)
-
+    y_sim_z[C1_idx] <- .split_chars(yC1_sim[1])
+    # The complete graph
+    if( length(S_marginals) == 1L ) return(y_sim_z)
     for( k in 2:length(C_marginals) ) {
-      nCk  <- C_marginals[[k]]
-      nSk  <- S_marginals[[k]]
-      Ck   <- names(dimnames(nCk)) # Clique names
-      Sk   <- names(dimnames(nSk)) # Separator names
-
-      Ck_index <- ia(Delta, names(dimnames(nCk)))
-      Sk_index <- ia(Delta, names(dimnames(nSk)))
-
-      ySk      <- y_sim_z[matrix(Sk_index, 1)]
-      nSk_ySk  <- na_ya(Delta, nSk, y_sim_z)
-
-      Ck_minus_Sk_levels  <- dimnames(nCk)[-which(Ck %in% Sk)]
-      ICk_minus_Sk        <- do.call(.expand_grid, Ck_minus_Sk_levels)
-      N_ick_minus_sk      <- 1:nrow(ICk_minus_Sk)
-      
-      indexC              <- which(!(Ck %in% Sk))
-      nCk_given_nSk       <- apply(nCk, indexC, function(x) x[matrix(ySk, 1)])
-
-      p_nCk_given_nSk     <- nCk_given_nSk / nSk_ySk ## Can this be zero? Make an ifelse(t, y, n)
-      yCk_minus_ySk_sim_z <- sample(N_ick_minus_sk, 1, prob = p_nCk_given_nSk)
-      Ck_minus_Sk_index   <- Ck_index[-which(Ck_index %in% Sk_index)]
-      y_sim_z[Ck_minus_Sk_index] <- unlist(ICk_minus_Sk[yCk_minus_ySk_sim_z, ])
+      nCk     <- C_marginals[[k]]
+      Ck_vars <- attr(nCk, "vars")     # Clique names
+      Ck_idx  <- match(Ck_vars, Delta) # Where is Ck in Delta
+      nSk     <- S_marginals[[k]]      # For Sk = Ø we have that nSk = M
+      Sk_vars <- attr(nSk, "vars")     # Separator names
+      # For empty separators
+      if( is.null(Sk_vars) ) {
+        p_nCk_minus_nSk <- nCk / nSk
+        y_sim_z[Ck_idx] <- .split_chars(sample(names(p_nCk_minus_nSk), 1L, prob = p_nCk_minus_nSk))
+      } else {
+        Sk_idx              <- match(Sk_vars, Delta)
+        Sk_idx_in_Ck        <- match(Sk_vars, Ck_vars)
+        Ck_idx_minus_Sk_idx <- Ck_idx[-Sk_idx_in_Ck]
+        ySk                 <- y_sim_z[Sk_idx]
+        nSk_ySk             <- na_ya(nSk, ySk)
+        nCk_given_Sk        <- na_b(nCk, structure(Sk_idx_in_Ck, names = ySk) )
+        p_nCk_given_Sk_ySk  <- nCk_given_Sk / nSk_ySk # Cant be Inf, since ySk MUST be present since we simulated it
+        y_sim_z[Ck_idx_minus_Sk_idx] <- .split_chars(sample(names( p_nCk_given_Sk_ySk), 1L, prob =  p_nCk_given_Sk_ySk))
+      }
     }
-    y_sim_z  
-  })
+    # structure(y_sim_z, names = Delta)
+    TY(structure(y_sim_z, names = Delta), C_marginals, S_marginals)
+  }
+  doParallel::stopImplicitCluster()
   y
 }
 
-sim_TY <- function(df,
-                   C_marginals,
-                   S_marginals,
-                   n.sim = 1000,
-                   ncores = 1,
-                   verbose = TRUE) {
-  # OUTPUT:
-  # Simulated values of T(Y) from the database df
-  y_sim <- sim_Y(df,
-    C_marginals,
-    S_marginals,
-    n.sim,
-    ncores,
-    verbose
-  )
-
-  if(verbose) cat("\nCalculating T(y)...\n")
-  if(!verbose) pbapply::pboptions(type = "none")
-  on.exit(pbapply::pboptions(type = "timer"))
-
-  pbapply::pbsapply(X = y_sim,
-    cl = ncores,
-    FUN = function(y) T_Y(df, y, C_marginals,S_marginals)
-  ) 
-}
+## simulate_TY <- function(df,
+##                    C_marginals,
+##                    S_marginals,
+##                    n.sim = 1000,
+##                    ncores = 1,
+##                    verbose = TRUE) {
+##   # OUTPUT:
+##   # Simulated values of T(Y) from the database df
+##   sim_cell <- simulate_cell(df, C_marginals, S_marginals, n.sim, ncores, verbose)
+##   sapply(sim_cell, TY, C_marginals, S_marginals) 
+## }
