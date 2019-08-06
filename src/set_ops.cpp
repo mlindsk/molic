580de53105cd4464f5c148866932b944f072b579
Mads <mads@math.aau.dk>
2019-08-06 09:39:19 +0200

    rm redundant lines

--- src/set_ops.cpp
+++ src/set_ops.cpp
@@ -5,49 +5,49 @@
  *                  FOR EFS C++ VERSION                                   
  * ---------------------------------------------------------*/
 
-set set_intersect(set &v1, set &v2) {
-  // Tjeck which is the smallest
-  set v;
-  for (auto i = v1.begin(); i != v1.end(); i++) {
-    if (v2.find(*i) != v2.end()) v.insert(*i);
-  }
-  return v;
-}
+// set set_intersect(set &v1, set &v2) {
+//   // Tjeck which is the smallest
+//   set v;
+//   for (auto i = v1.begin(); i != v1.end(); i++) {
+//     if (v2.find(*i) != v2.end()) v.insert(*i);
+//   }
+//   return v;
+// }
 
-set set_union(set &v1, set &v2) {
-  // Tjeck which is the smallest
-  for (auto i = v2.begin(); i != v2.end(); i++) {
-    if (v1.find(*i) != v1.end()) v1.insert(*i);
-  }
-  return v1;
-}
+// set set_union(set &v1, set &v2) {
+//   // Tjeck which is the smallest
+//   for (auto i = v2.begin(); i != v2.end(); i++) {
+//     if (v1.find(*i) != v1.end()) v1.insert(*i);
+//   }
+//   return v1;
+// }
 
-set set_diff(set &v1, set &v2) {
-  set v = v2;
-  for (auto i = v.begin(); i != v.end(); i++) {
-    if (v1.find(*i) != v1.end()) v.erase(*i);
-  }
-  return v;
-}
+// set set_diff(set &v1, set &v2) {
+//   set v = v2;
+//   for (auto i = v.begin(); i != v.end(); i++) {
+//     if (v1.find(*i) != v1.end()) v.erase(*i);
+//   }
+//   return v;
+// }
 
-bool set_equal(set &v1, set &v2) {
-  if(v1.size() != v2.size()) return false;
-  return std::equal(std::begin(v1), std::end(v1), std::begin(v2));
-}
+// bool set_equal(set &v1, set &v2) {
+//   if(v1.size() != v2.size()) return false;
+//   return std::equal(std::begin(v1), std::end(v1), std::begin(v2));
+// }
 
-bool set_in(const std::string & a, const set & b) {
-  return b.find(a) != b.end();
-}
+// bool set_in(const std::string & a, const set & b) {
+//   return b.find(a) != b.end();
+// }
 
-bool set_issubeq(set & a, set & b) {
-  if ( a.size() > b.size () ) return false;
-  for (auto & e : a) {
-    if( b.find(e) == b.end() ) {
-      return false;
-    }    
-  }
-  return true;
-}
+// bool set_issubeq(set & a, set & b) {
+//   if ( a.size() > b.size () ) return false;
+//   for (auto & e : a) {
+//     if( b.find(e) == b.end() ) {
+//       return false;
+//     }    
+//   }
+//   return true;
+// }
 
 /*----------------------------------------------------------*
  *                     FOR OUTLIER UTILS                                   
--- src/set_ops.h
+++ src/set_ops.h
@@ -12,12 +12,12 @@ using VS  = std::vector<std::string>;
  * For efs c++ version
  */
 
-set  set_intersect(set &v1, set &v2);
-set  set_union(set &v1, set &v2);
-set  set_diff(set &v1, set &v2);
-bool set_equal(set &v1, set &v2);
-bool set_in(const std::string & a, const set & b);
-bool set_issubeq(set & a, set & b);
+// set  set_intersect(set &v1, set &v2);
+// set  set_union(set &v1, set &v2);
+// set  set_diff(set &v1, set &v2);
+// bool set_equal(set &v1, set &v2);
+// bool set_in(const std::string & a, const set & b);
+// bool set_issubeq(set & a, set & b);
 
 /**
  * For outlier_utils
